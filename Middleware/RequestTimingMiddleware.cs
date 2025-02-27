27817d868a565a0848f09b18b8679143e1bf5786
sachin modi <parisachi@outlook.com>
2025-02-27 17:42:05 +0530

    Added RequestTimingMiddleware

---
+++ Middleware/RequestTimingMiddleware.cs
@@ -0,0 +1,26 @@
+﻿namespace EmployeeManagementCore.Middleware
+{
+    using Microsoft.AspNetCore.Http;
+    using System.Diagnostics;
+    using System.Threading.Tasks;
+
+    public class RequestTimingMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        public RequestTimingMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task InvokeAsync(HttpContext context)
+        {
+            var stopwatch = Stopwatch.StartNew();
+            await _next(context);
+            stopwatch.Stop();
+
+            var elapsedTime = stopwatch.ElapsedMilliseconds;
+            context.Response.Headers.Add("X-Processing-Time-Milliseconds", elapsedTime.ToString());
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,10 +1,12 @@
+using EmployeeManagementCore.Middleware;
 using EmployeeManagementCore.Models;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.EntityFrameworkCore;
 
+// used to initialize and configure an ASP.NET Core web application using the Minimal Hosting Model.
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+// Add services or Register services to the container.
 builder.Services.AddControllersWithViews();
 
 // address dbcontext to the container.
@@ -13,7 +15,9 @@ builder.Services.AddDbContext<EmployeeDbContext>(options =>
 
 builder.Services.AddSwaggerGen();
 
+// Build the application (finalizes the configuration)
 var app = builder.Build();
+
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
@@ -34,5 +38,8 @@ app.UseAuthorization();
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
+
+// Register Middleware
+app.UseMiddleware<RequestTimingMiddleware>(); 
 
 app.Run();
